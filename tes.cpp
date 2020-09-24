abcb17f05ac93b0d81b79c0eda28d6f6c5475569
ovisannn <ovi.sanjaya11@gmail.com>
2020-09-24 07:44:09 +0700

    coba lagi

--- tes.cpp
+++ tes.cpp
@@ -1,13 +1,21 @@
 # include <iostream>
 using namespace std;
-
+int a = 0;
+bool konfirm=true;
 
 int main(){
 
 
 for(int i=0;i<=5;i++){
-        cout<<i<<endl;
+cout<<i<<endl;
+} //tag penutup for
 
+while (konfirm == true){
+    cout<<"kalimat ini akan terulan selama i belum mencapai batas maksimal"<<endl;
+    a = a+1;
+    if(a==10){
+        konfirm = false;
+    }
 }
 
-    }
+}//tag penutup utama
